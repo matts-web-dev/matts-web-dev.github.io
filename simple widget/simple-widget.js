29eab4c68c9a9b7349153b94683302a9e0219a60
matts-web-dev <matt.steckbauer@gmail.com>
2014-10-28 15:00:05 -0700

    Update simple-widget.js

--- simple widget/simple-widget.js
+++ simple widget/simple-widget.js
@@ -3,7 +3,7 @@ var $container = '#NamefoWidget';
 
 var $Css = '<link rel="stylesheet" href="http://matts-web-dev.github.io/simple%20widget/widget.css" type="text/css" />';
 
-var $Html ='<div id="widget_shout"><p>New post at <a target="_blank" href="http://mattswebdev.tk/Forum/index.php">Matts web dev</a> on <a href="#">Html and css</a></p><div>';
+var $Html ='<div id="widget_shout"><p>New post at <a target="_blank" href="http://mattswebdev.tk/Forum/index.php">Matts web dev</a> on <a href="http://mattswebdev.tk/Forum/viewforum.php?f=1">Jquery or Javascript</a></p><div>';
 
 
 $('head').append($Css);
