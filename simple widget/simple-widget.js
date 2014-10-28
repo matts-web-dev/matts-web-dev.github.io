770780aef32781ce547f137e84f0da2c513f3d65
mattbtw <matt.steckbauer@gmail.com>
2014-10-28 14:50:13 -0700

    simple-widget.js

---
+++ simple widget/simple-widget.js
@@ -0,0 +1,8 @@
+
+var $Css = '<link rel="stylesheet" href="http://matts-web-dev.github.io/simple%20widget/widget.css" type="text/css" />';
+
+var $Html ='<div id="widget_shout"><p>New guide up at <a target="_blank" href="http://mattswebdev.tk/Forum/index.php">Matts web dev</a> on <a href="#">Html and css</a></p><div>';
+
+
+$('head').append($Css);
+$($widgetdiv).append($Html);
